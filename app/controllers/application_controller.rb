7f2448a0f6d859eb212d28f8dd26442739a1d1d6
Jason Liu <jason@jasonkliu.com>
2014-08-23 16:27:25 -0400

    Set access headers to all (todo: revert)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,22 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :null_session
+  # protect_from_forgery with: :null_session
 
   # Add this before action to force CAS Authentication on all controllers + actions
   before_action CASClient::Frameworks::Rails::Filter, unless: :skip_login?
 
   # Add this before action to set a local variable for the current user from CAS session
   before_action :getMe
+
+  after_filter :set_access_control_headers
+
+  def set_access_control_headers 
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
+    headers['Access-Control-Request-Method'] = '*'
+    headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  end
 
   protected
 
