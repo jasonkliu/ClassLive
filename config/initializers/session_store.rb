e844c117870c5559335326c25e542ca757574d52
Jason Liu <jason@jasonkliu.com>
2014-08-23 10:55:32 -0400

    encrypted_cookie_store to cookie_store

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-ClassLive::Application.config.session_store :encrypted_cookie_store, key: '_ClassLive_session'
+ClassLive::Application.config.session_store :cookie_store, key: '_ClassLive_session'
