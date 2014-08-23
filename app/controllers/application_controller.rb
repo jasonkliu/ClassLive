dbe41f6870cd0e52d09f58a5cd9212ae1f626606
Jason Liu <jason@jasonkliu.com>
2014-08-23 12:10:01 -0400

    Add CAS additional auth files, hashsyntax

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,12 +3,25 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  before_filter RubyCAS::Filter
-  before_filter :current_user
-  # so we can use current_user in the views
-  helper_method :current_user
+  # Add this before filter to force CAS Authentication on all controllers + actions
+  before_filter CASClient::Frameworks::Rails::Filter, unless: :skip_login?
 
-  def current_user
-    @current_user ||= User.find_by_login(session[:cas_user]) if session[:cas_user]
+  # Add this before filter to set a local variable for the current user from CAS session
+  before_filter :getMe
+
+  protected
+
+  def getMe
+    @me = User.find_or_create_by_netid(netid: session[:cas_user] )
+    if !@me
+      redirect_to :root
+      return false
+    end
+  end
+
+  # hack for skip_before_filter with CAS
+  # overwrite this method (with 'true') in any controller to skip CAS auth
+  def skip_login?
+    false
   end
 end
