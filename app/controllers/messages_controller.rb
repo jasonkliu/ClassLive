dea08fda1977216771c84864b9c69be80c1fc476
Jason Liu <jason@jasonkliu.com>
2014-08-23 14:56:02 -0400

    Create new message

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,2 +1,11 @@
 class MessagesController < ApplicationController
+  def create(course_id, content)
+    @msg = Message.new(course_id: course_id, content: content,
+                       netid: current_user.netid, created_at: Time.now,
+                       updated_at: Time.now)
+    respond_to do |format|
+      format.html { render json: @msg.to_json }
+      format.json { render json: @msg.to_json }
+    end
+  end
 end
