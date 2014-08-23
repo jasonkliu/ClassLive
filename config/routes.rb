654187a79398c3006c672c2ced3615aea7a464b7
Jason Liu <jason@jasonkliu.com>
2014-08-23 14:36:14 -0400

    Add messages routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 ClassLive::Application.routes.draw do
   resources :users
+  resources :messages
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
