b41dda1a3bac4f9d775373b2351c4b8daff48dc5
Minh Tri Pham <minhtri@pham.cz>
2014-08-23 14:45:16 -0400

    final fix

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -2,6 +2,7 @@ class ChatsController < ApplicationController
 	def index
 		@chats = Chat.all
 		respond_to do |format|
+			format.html { render :json => @chats.to_json }
 			format.json { render :json => @chats.to_json }
 		end
 	end
